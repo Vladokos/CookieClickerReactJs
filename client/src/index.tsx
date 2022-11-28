206d55985bb7f9fe6ffcc17b1d8610a4217d4e4e
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-11-29 01:15:40 +0400

    migrate from js to ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,39 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import product from "./interface/product";
 
-class InitialMenu extends React.Component{
-    
+type Props = {};
+
+type State = {
+  currency: number;
+  storeIsOpen: boolean;
+  helpers: number;
+  products: Array<product>;
+};
+
+class InitialMenu extends React.Component<Props, State> {
+  state: State = {
+    currency: 0,
+    storeIsOpen: false,
+    helpers: 0,
+    products: [],
+  };
+
+  // componentDidMount() {
+  //   fetch("/api/products")
+  //     .then((res) => res.json())
+  //     .then((products: product) => {
+
+  //     });
+  // }
+
+  clickOnCookie() {
+    this.setState((state) => ({
+      currency: state.currency + 1,
+    }));
+  }
+
+  render(): React.ReactNode {
+    return <div></div>;
+  }
 }
