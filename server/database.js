ac62f8540618cb552aafbfa52cc47583ef13f68a
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-12-29 23:18:36 +0400

    database commands

--- server/database.js
+++ server/database.js
@@ -1,25 +1,23 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const database = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "root",
   database: "cookieclicker",
 });
-
 // const open = connection.connect((err) => {
 //   if (err) {
 //     return console.log("Error: " + err.message);
 //   }
 //   console.log("Success");
 // });
-
-// const close = connection.close((err) => {
+// const close = connection.end((err) => {
 //   if (err) {
 //     return console.log("Error: " + err.message);
 //   }
 //   console.log("Success");
 // });
 
-module.exports = { connection };
+module.exports = { database };
