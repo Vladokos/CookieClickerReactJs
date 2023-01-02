ea3864f9d540316e88581d79cb50a72b4a24ef24
Vlad <66181660+Vladokos@users.noreply.github.com>
2023-01-03 02:10:09 +0400

    test

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const { database } = require("./database.js");
 
+const path = require('path');
+
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.get("/api/products", (req, res) => {
   database.query("SELECT * FROM products", (error, results, fields) => {
