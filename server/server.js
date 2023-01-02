1603de757ae45d21e74f40b0507153b8465dd592
Vlad <66181660+Vladokos@users.noreply.github.com>
2023-01-02 23:41:59 +0400

    added queries

--- server/server.js
+++ server/server.js
@@ -1,20 +1,47 @@
-const {database} = require("./database.js");
+const { database } = require("./database.js");
 
 const express = require("express");
 const app = express();
 
 app.get("/api/products", (req, res) => {
-   
-  let products;
   database.query("SELECT * FROM products", (error, results, fields) => {
-    products = results;
-  })
+    return res.status(200).json(results);
+  });
+});
 
-  res.status(200).json(products);
+app.post("/api/getData", (req, res) => {
+  const { id } = req.body;
+
+  database.query(
+    `SELECT amountCookies,amountHelpers FROM users WHERE id = '${id}'`,
+    (error, results, fields) => {
+      return res.status(200).json(results);
+    }
+  );
+});
+
+app.post("/api/writeData", (req, res) => {
+  console.log(req.body);//fix 'cause get undefined
+  const { id, currency, helpers } = req.body;
+  database.query(
+    `SELECT * FROM users WHERE id = '${id}'`,
+    (error, results, fields) => {
+      if (results.length > 0) {
+        database.query(`UPDATE users SET amountCookies = '${currency}', 
+      amountHelpers = '${helpers}' WHERE id = '${id}'`);
+      } else {
+        database.query(
+          `INSERT INTO users VALUES('${id}','${currency}','${helpers}')`
+        );
+        database.query(`UPDATE users SET amountCookies = '${currency}', 
+      amountHelpers = '${helpers}' WHERE id = '${id}'`);
+      }
+    }
+  );
+
+ return res.status(200);
 });
 
 app.listen(5000, () => {
   console.log("Server is waiting");
 });
-
-// add sql
